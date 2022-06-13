6d845fcb483e3d95132fd2301152a86c3f3ab9cd
napalm17 <suerberkay17@gmail.com>
2022-06-13 16:59:17 +0200

    A

--- src/A/LucasSeries.java
+++ src/A/LucasSeries.java
@@ -1,12 +1,31 @@
 package A;
 
+/**
+ * Computes a Lucas-Series with the given parameters and range.
+ * @author ugpsy
+ * @version 1.0
+ */
 public class LucasSeries {
+
+    /**
+     * Constructor to avoid errors.
+     */
+    public LucasSeries() {
+    }
+
+    /**
+     * Computes elements of a Lucas-Series.
+     * @param n The range of the series. (0-n).
+     * @param p Freely chosen parameter.
+     * @param q Another Freely chosen parameter.
+     * @return an integer array that contains the elements of the Lucas-Series.
+     */
     public long[] getSeries(int n, int p, int q) {
         long[] l = new long[n];
-        l[0] = 0;
+        l[0] = 0; // Set the first two elements manually.
         l[1] = 1;
         for (int i = 2; i < n; i++) {
-            l[i] = p * l[i - 1] - q * l[i - 2];
+            l[i] = p * l[i - 1] - q * l[i - 2]; // Apply the given formula.
         }
         return l;
     }
--- src/A/Main.java
+++ src/A/Main.java
@@ -1,8 +1,24 @@
 package A;
 
+/**
+ * The Main Class.
+ * @author ugpsy
+ * @version 1.0
+ */
 public class Main {
+
+    /**
+     * Constructor to avoid errors.
+     */
+    public Main() {
+    }
+
+    /**
+     * The main method.
+     * All the errors are handled here.
+     */
     public static void main(String[] args) {
-        try {
+        try {                                     // Raise an error, if one of the arguments is not an integer.
             for (String argument: args) {
                 Integer.parseInt(argument);
             }
@@ -10,19 +26,29 @@ public class Main {
             System.out.println("Error: Command line arguments must be integers.");
             return;
         }
-        if (args.length != 3) {
+        if (args.length != 3) {                   // Raise an error, if the number of given arguments is not 3.
             System.out.println("Error: Enter exactly 3 command line arguments.");
         }
-        else if (Integer.parseInt(args[0]) < 0) {
+        else if (Integer.parseInt(args[0]) < 0) { // Raise an error, if n is a negative integer.
             System.out.println("Error: The integer n must be greater than or equal to 0.");
 
         }
-        else {
-            LucasSeries lucasSeries = new LucasSeries();
-            long[] series = lucasSeries.getSeries(Integer.parseInt(args[0]) + 1, Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-            for (long element : series) {
-                System.out.println(element);
-            }
+        else {                                    // Run the program, if no error has been raised.
+            run(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+        }
+    }
+
+    /**
+     * The method that runs the algorithm for finding the Lucas series.
+     * @param n The range of the series. (0-n).
+     * @param p Freely chosen parameter.
+     * @param q Another Freely chosen parameter.
+     */
+    private static void run(int n , int p , int q) {
+        LucasSeries lucasSeries = new LucasSeries();
+        long[] series = lucasSeries.getSeries(n + 1, p, q); // n+1 because the nth element is included in the series.
+        for (long element : series) {
+            System.out.println(element);
         }
     }
 }
