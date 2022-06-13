cfd07667528a27d0043013d8a7be2f43e3f01be0
napalm17 <suerberkay17@gmail.com>
2022-06-13 16:37:57 +0200

    A

---
+++ src/LucasSeries.java
@@ -0,0 +1,11 @@
+public class LucasSeries {
+    public long[] getSeries(int n, int p, int q) {
+        long[] l = new long[n];
+        l[0] = 0;
+        l[1] = 1;
+        for (int i = 2; i < n; i++) {
+            l[i] = p * l[i - 1] - q * l[i - 2];
+        }
+        return l;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            for (String argument: args) {
+                Integer.parseInt(argument);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Command line arguments must be integers.");
+            return;
+        }
+        if (args.length != 3) {
+            System.out.println("Error: Enter exactly 3 command line arguments.");
+        }
+        else if (Integer.parseInt(args[0]) < 0) {
+            System.out.println("Error: The integer n must be greater than or equal to 0.");
+
+        }
+        else {
+            LucasSeries lucasSeries = new LucasSeries();
+            long[] series = lucasSeries.getSeries(Integer.parseInt(args[0]) + 1, Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            for (long element : series) {
+                System.out.println(element);
+            }
+        }
+    }
+}
